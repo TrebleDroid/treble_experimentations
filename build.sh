5732cff8c08e0d6200713bff58fd30f606de6b8a
Jonathon Fitch <lightman1@virginmedia.com>
2023-04-01 02:04:30 +0100

    Change manifest location for Gapps

--- build.sh
+++ build.sh
@@ -27,10 +27,11 @@ repo init -u "$manifest_url" -b $aosp --depth=1
 if [ -d .repo/local_manifests ] ;then
 	( cd .repo/local_manifests; git fetch; git reset --hard; git checkout origin/$phh)
 else
-	git clone https://github.com/TrebleDroid/treble_manifest .repo/local_manifests -b $phh
+	git clone https://github.com/JonnyVR1/treble_manifest .repo/local_manifests -b $phh
 fi
-repo sync -c -j 1 --force-sync || repo sync -c -j1 --force-sync
+repo sync -c -j8 --force-sync || repo sync -c -j8 --force-sync
 
+repo forall -r '.*opengapps.*' -c 'git lfs fetch && git lfs checkout'
 (cd device/phh/treble; git clean -fdx; bash generate.sh)
 
 . build/envsetup.sh
@@ -48,14 +49,21 @@ bash "$originFolder"/list-patches.sh
 cp patches.zip release/$rom_fp/patches-for-developers.zip
 
 (
-    git clone https://github.com/TrebleDroid/sas-creator
-    cd sas-creator
+	if [ -d sas-creator ] ;then
+		( cd sas-creator; git fetch; git reset --hard)
+	else
+		git clone https://github.com/TrebleDroid/sas-creator
+		cd sas-creator
+	fi
 
-    git clone https://github.com/phhusson/vendor_vndk -b android-10.0
+	if [ -d vendor_vndk ] ;then
+		( cd vendor_vndk; git fetch; git reset --hard; cd ../)
+	else
+		git clone https://github.com/phhusson/vendor_vndk -b android-10.0
+	fi
 )
 
-buildVariant treble_arm64_bvS$supp-userdebug td-arm64-ab-vanilla
-( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-td-arm64-ab-vndklite-vanilla.img.xz )
-
-buildVariant treble_a64_bvS$supp-userdebug td-arm32_binder64-ab-vanilla
-( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-td-arm32_binder64-ab-vndklite-vanilla.img.xz )
+buildVariant treble_arm64_bgN$supp-userdebug td-arm64-ab-gapps
+( cd sas-creator; bash securize.sh $OUT/system.img; xz -c s-secure.img -T0 > ../release/$rom_fp/system-td-arm64-ab-gapps-secure.img.xz )
+( cd sas-creator; bash lite-adapter.sh 64 $OUT/system.img; xz -c s.img -T0 > ../release/$rom_fp/system-td-arm64-ab-vndklite-gapps.img.xz )
+( cd sas-creator; bash securize.sh s.img; xz -c s-secure.img -T0 > ../release/$rom_fp/system-td-arm64-ab-vndklite-gapps-secure.img.xz )
